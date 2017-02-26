e056d41851eb171e1d8d678df2894fad38dd0351
zhaorui <bupt_zhaorui@foxmail.com>
2017-02-26 22:56:27 +0800

    maximal squares

---
+++ test/__init__.py
@@ -0,0 +1,13 @@
+count = 0
+while count < 9:
+    print('The count is:', count)
+    count += 1
+    print("d")
+
+print("Good bye!")
+
+
+map(lambda x: x**2, [1, 2, 3, 4, 5])
+
+list = [1, 3, 4, 5]
+
