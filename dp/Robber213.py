a81f0f486514fa9fea9e277c2729648b8f6ecc99
zhaorui <bupt_zhaorui@foxmail.com>
2017-02-27 11:47:47 +0800

    maximal squares

--- dp/Robber213.py
+++ dp/Robber213.py
@@ -4,32 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp1, dp2 = [0 for i in range(len(nums))], [0 for i in range(len(nums))]
-        maxNum = 0
+        n = len(nums)
+        if n == 0: return 0
+        elif n == 1: return nums[0]
+        elif n == 2: return max(nums[0], nums[1])
+        else:
+            dp, maxNum = [[0 for _1_ in range(n)] for _2_ in [0, 1]], 0
 
-        for i in range(len(nums)):
-            if i == 0:
-                dp1[i] = nums[i]
-                dp2[i] = 1
-            elif i == 1:
-                dp1[i] = nums[i]
-            elif i == len(nums) - 1:
-                if i-2 >= 0 and dp2[i-2] == 0:
-                    dp1[i] = max(dp1[i], dp1[i-2] + nums[i])
-                elif i-3 >= 0 and dp2[i-3] == 0:
-                    dp1[i] = max(dp1[i], dp1[i-3] + nums[i])
-                else:
-                    dp1[i] = nums[i]
-            else:
-                if dp1[i-2] > dp1[i-3]:
-                    dp1[i] = dp1[i-2] + nums[i]
-                    dp2[i] = dp2[i-2]
-                elif dp1[i-2] == dp1[i-3]:
-                    dp1[i] = dp1[i-2] + nums[i]
-                    dp2[i] = dp2[i-2] & dp2[i-3]
-                else:
-                    dp1[i] = dp1[i-3] + nums[i]
-                    dp2[i] = dp2[i-3]
-            maxNum = max(maxNum, dp1[i])
+            dp[1][0] = nums[0]
+            dp[0][1] = nums[1]
+            maxNum = max(dp[1][0], dp[0][1])
 
-        return maxNum
+            for i in [0, 1]:
+                for j in range(2, n):
+                    if j-3 >= 0:
+                        dp[i][j] = max(dp[i][j], dp[i][j-2] + nums[j], dp[i][j-3] + nums[j])
+                    else:
+                        dp[i][j] = max(dp[i][j], dp[i][j-2] + nums[j])
+
+                    if not (i == 1 and j == n-1):
+                        maxNum = max(maxNum, dp[i][j])
+
+            return maxNum
